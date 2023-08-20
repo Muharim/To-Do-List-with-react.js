921d1bbaa0c6118023f83f2a96c420722a4c23ec
Muharim <muharim508@gmal.com>
2023-08-20 22:37:28 +0700

    update UI

--- src/App.jsx
+++ src/App.jsx
@@ -21,11 +21,11 @@ function App() {
 
   return (
     <div className="bg-gray-300 w-full h-screen flex items-center">
-      <div className="m-[500px] mx-auto text-center bg-white p-5 rounded-lg">
+      <div className="m-[500px] mx-auto text-center bg-[#fdba74] p-5 rounded-lg">
         <h1 className="text-5xl font-bold mb-8">Todo List</h1>
         <form onSubmit={handleForm}>
-          <input className="border-2 placeholder:text-gray-500 border-black rounded-lg w-full p-5 mb-5 text-black" type="text" placeholder="Add Todo" value={todo} onChange={(e) => setTodo(e.target.value)}/>
-          <button type="submit" className="bg-green-600 text-white py-3 px-8 rounded-lg mb-8">Add</button>
+          <input className="border-2 placeholder:text-gray-500 border-black hover:border-gray-500 rounded-lg w-full p-5 mb-5 text-black" type="text" placeholder="Add Todo" value={todo} onChange={(e) => setTodo(e.target.value)}/>
+          <button type="submit" className="bg-green-500 hover:bg-green-600 text-white py-3 px-8 rounded-lg mb-8">Add</button>
         </form>
         <div className="todo-show-area">
           <ul>
@@ -33,9 +33,9 @@ function App() {
               return (
                 <li 
                 key={index}
-                className="bg- mb-5 flex justify-between text-white py-5 rounded-lg px-5 text-3xl">
+                className="bg-white mb-5 flex justify-between text-green-700 py-5 rounded-lg px-5 text-3xl">
                 {singleTodo.todoName}{" "}
-                <span onClick={() => deleteTodo(singleTodo.todoName)} className="text-red-600 text-3xl cursor-pointer">X</span>
+                <span onClick={() => deleteTodo(singleTodo.todoName)} className="text-black-600 hover:text-red-600 text-3xl cursor-pointer">X</span>
                 </li>
               );
             })}
