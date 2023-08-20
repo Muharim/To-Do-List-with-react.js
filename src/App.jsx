1b829d16d0355c11b7645693a8ed4f7210c53063
Muharim <muharim508@gmal.com>
2023-08-20 22:12:58 +0700

    create To do List

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react"
+
+function App() {
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    setTodoList([...todoList, { todoName: todo }]);
+    setTodo("");
+  };
+
+  const deleteTodo = (deleteValue) => {
+    const restTodoList = [
+      ...todoList.filter((val) => {
+      return val.todoName != deleteValue;
+      }),
+    ];
+    setTodoList(restTodoList);
+  };
+
+  return (
+    <div className="bg-gray-300 w-full h-screen flex items-center">
+      <div className="m-[500px] mx-auto text-center bg-white p-5 rounded-lg">
+        <h1 className="text-5xl font-bold mb-8">Todo List</h1>
+        <form onSubmit={handleForm}>
+          <input className="border-2 placeholder:text-gray-500 border-black rounded-lg w-full p-5 mb-5 text-black" type="text" placeholder="Add Todo" value={todo} onChange={(e) => setTodo(e.target.value)}/>
+          <button type="submit" className="bg-green-600 text-white py-3 px-8 rounded-lg mb-8">Add</button>
+        </form>
+        <div className="todo-show-area">
+          <ul>
+            {todoList.map((singleTodo, index) => {
+              return (
+                <li 
+                key={index}
+                className="bg- mb-5 flex justify-between text-white py-5 rounded-lg px-5 text-3xl">
+                {singleTodo.todoName}{" "}
+                <span onClick={() => deleteTodo(singleTodo.todoName)} className="text-red-600 text-3xl cursor-pointer">X</span>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App
